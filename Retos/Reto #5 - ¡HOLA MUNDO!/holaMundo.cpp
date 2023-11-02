8727cfb3450545ed505e254a3b72d6a7ee45bcd8
s-bauza <s.bauza@alumnos.upm.es>
2023-11-02 11:54:33 +0100

    Terminado reto #5

---
+++ Retos/Reto #5 - ¡HOLA MUNDO!/holaMundo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "¡Hola Mundo!";
+    return 0;
+}
---
+++ Retos/Reto #5 - ¡HOLA MUNDO!/holaMundo.java
@@ -0,0 +1,6 @@
+public class holaMundo {
+
+    public static void main(String[] args) {
+        System.out.print("¡Hola mundo!");
+    }
+}
